04039d2dbec027273e1c2a863a0268195818b137
Nico Nel <niconel@gmail.com>
2023-06-07 23:15:58 +0200

    End of section 4

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,21 +11,48 @@ import {
   Stack,
   useColorModeValue,
   useColorMode,
-} from "@chakra-ui/react";
-import { Link as ReactLink } from "react-router-dom";
-import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
-import { GiTechnoHeart } from "react-icons/gi";
+  LinkBox,
+} from '@chakra-ui/react';
+import { Link as ReactLink } from 'react-router-dom';
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { GiTechnoHeart } from 'react-icons/gi';
 
+const NavLink = ({ path, children }) => (
+  <Link
+    as={ReactLink}
+    to={path}
+    px={2}
+    py={2}
+    rounded="md"
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+  >
+    {children}
+  </Link>
+);
+
+const links = [
+  {
+    linkName: 'Products',
+    path: '/products',
+  },
+  {
+    linkName: 'Shopping Cart',
+    path: '/cart',
+  },
+];
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Box bg={useColorModeValue("gray.100", "gray.900")} p={4}>
-      <Flex h={6} alignItems={"center"} justifyContent={"space-between"}>
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} p={4}>
+      <Flex h={6} alignItems={'center'} justifyContent={'space-between'}>
         <IconButton
           size="md"
           icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          display={{ md: "none" }}
+          display={{ md: 'none' }}
           onClick={isOpen ? onClose : onOpen}
         />
         <HStack>
@@ -35,8 +62,68 @@ const Navbar = () => {
               <Text fontWeight="extrabold">Tech Lines</Text>
             </Flex>
           </Link>
+
+          <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }}>
+            {links.map((link) => (
+              <NavLink
+                key={link.linkName}
+                path={link.path}
+                children={link.linkName}
+              />
+            ))}
+          </HStack>
         </HStack>
+        <Flex alignItems="center">
+          <NavLink>
+            <Icon
+              as={colorMode === 'light' ? MoonIcon : SunIcon}
+              alignSelf="center"
+              onClick={() => toggleColorMode()}
+            />
+          </NavLink>
+
+          <Button
+            as={ReactLink}
+            to="/login"
+            p={2}
+            fontSize="sm"
+            fontWeight={400}
+            variant="link"
+          >
+            Sign In
+          </Button>
+
+          <Button
+            as={ReactLink}
+            to="/registration"
+            m={2}
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize="sm"
+            fontWeight={600}
+            _hover={{ bg: 'orange.400' }}
+            bg="orange.500"
+            color="white"
+          >
+            Sign Up
+          </Button>
+        </Flex>
       </Flex>
+      {isOpen ? (
+        <Box pb={4} display={{ md: 'none' }}>
+          <Stack as="nav" spacing={4}>
+            {links.map((link) => (
+              <NavLink
+                key={link.linkName}
+                path={link.path}
+                children={link.linkName}
+              />
+            ))}
+            <NavLink key="sign_up" path="/registration">
+              Sign Up
+            </NavLink>
+          </Stack>
+        </Box>
+      ) : null}
     </Box>
   );
 };
